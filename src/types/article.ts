34b8968656e6aec0f0038fd5f6bbb36516401748
Artem Yakunenko <artem.yakunenko@plus4u.net>
2023-01-14 16:44:25 +0200

    fix

--- src/types/article.ts
+++ src/types/article.ts
@@ -1,27 +1,26 @@
+import { ArticleModel } from '../models/Article'
+
 export interface ArticleState {
-    articles: any[];
-    loading: boolean;
-    error: null | string;
+  articles: ArticleModel[]
+  loading: boolean
+  error: null | string
 }
 
 export enum ArticleActionTypes {
-    FETCH_ARTICLE = "FETCH_ARTICLE",
-    FETCH_ARTICLE_SUCCESS = "FETCH_ARTICLE_SUCCESS",
-    FETCH_ARTICLE_ERROR = "FETCH_ARTICLE_ERROR",
+  FETCH_ARTICLE = 'FETCH_ARTICLE',
+  FETCH_ARTICLE_SUCCESS = 'FETCH_ARTICLE_SUCCESS',
+  FETCH_ARTICLE_ERROR = 'FETCH_ARTICLE_ERROR',
 }
 interface FetchArticleAction {
-    type: ArticleActionTypes.FETCH_ARTICLE;
+  type: ArticleActionTypes.FETCH_ARTICLE
 }
 interface FetchArticleSuccessAction {
-    type: ArticleActionTypes.FETCH_ARTICLE_SUCCESS;
-    payload: any[];
+  type: ArticleActionTypes.FETCH_ARTICLE_SUCCESS
+  payload: ArticleModel[]
 }
 interface FetchArticleErrorAction {
-    type: ArticleActionTypes.FETCH_ARTICLE_ERROR;
-    payload: string;
+  type: ArticleActionTypes.FETCH_ARTICLE_ERROR
+  payload: string
 }
 
-export type ArticleAction =
-    | FetchArticleAction
-    | FetchArticleErrorAction
-    | FetchArticleSuccessAction;
+export type ArticleAction = FetchArticleAction | FetchArticleErrorAction | FetchArticleSuccessAction

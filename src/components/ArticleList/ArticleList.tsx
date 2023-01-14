e82e29d7dd6f8839aebe19cb9e17bde262cd7843
Artem Yakunenko <artem.yakunenko@plus4u.net>
2023-01-14 16:36:12 +0200

    fix eslint errors, add eslint and prettier

--- src/components/ArticleList/ArticleList.tsx
+++ src/components/ArticleList/ArticleList.tsx
@@ -1,49 +1,48 @@
-import React, {useEffect} from 'react';
-import {useAppSelector} from "../../hooks/useTypedSelector";
-import {useActions} from "../../hooks/useActions";
-import {Box, CircularProgress, Grid} from "@mui/material";
-import ArticleCard from "../ArticleСard/ArticleСard";
-import {ArticleModel} from "../../models/Article";
+import React, { useEffect } from 'react'
+import { useAppSelector } from '../../hooks/useTypedSelector'
+import { useActions } from '../../hooks/useActions'
+import { Box, CircularProgress, Grid } from '@mui/material'
+import ArticleCard from '../ArticleСard/ArticleСard'
+import { ArticleModel } from '../../models/Article'
 
 interface ArticleListProps {
-    articlesToRender: ArticleModel[]
+  articlesToRender: ArticleModel[]
 }
-const ArticleList: React.FC<ArticleListProps> = ({articlesToRender}) => {
-    const { error, loading, articles } = useAppSelector((state) => state.articles)
-    const { fetchArticle } = useActions()
+const ArticleList: React.FC<ArticleListProps> = ({ articlesToRender }) => {
+  const { error, loading } = useAppSelector((state) => state.articles)
+  const { fetchArticle } = useActions()
 
-    console.log(articlesToRender)
+  console.log(articlesToRender)
 
-    useEffect(() => {
-        fetchArticle()
-    }, [])
+  useEffect(() => {
+    fetchArticle()
+  }, [])
 
-    if (loading) {
-        return (
-            <Box sx={{ display: 'flex' }}>
-                <CircularProgress />
-            </Box>
-        )
-    }
-    if (error) {
-        return <h1>{error}</h1>
-    }
-
+  if (loading) {
     return (
-        <Grid
-            container spacing={{ xs: 1, md: 3, lg: 4, xl: 5}}
-            columns={{ xs: 2, sm: 8, md: 12, lg: 14, xl: 18 }}
-            justifyContent="center"
-        >
-            {articlesToRender.map(article => (
-                <Grid
-                    item xs={2} sm={4} md={4} lg={4} xl={4}
-                    key={article.id}>
-                    <ArticleCard key={article.id} article={article}/>
-                </Grid>
-            ))}
-        </Grid>
-    );
-};
+      <Box sx={{ display: 'flex' }}>
+        <CircularProgress />
+      </Box>
+    )
+  }
+  if (error) {
+    return <h1>{error}</h1>
+  }
 
-export default ArticleList;
+  return (
+    <Grid
+      container
+      spacing={{ xs: 1, md: 3, lg: 4, xl: 5 }}
+      columns={{ xs: 2, sm: 8, md: 12, lg: 14, xl: 18 }}
+      justifyContent='center'
+    >
+      {articlesToRender.map((article) => (
+        <Grid item xs={2} sm={4} md={4} lg={4} xl={4} key={article.id}>
+          <ArticleCard key={article.id} article={article} />
+        </Grid>
+      ))}
+    </Grid>
+  )
+}
+
+export default ArticleList
--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,67 +1,52 @@
-import React, {useCallback, useState} from 'react';
-import ArticleList from "../ArticleList/ArticleList";
-import SearchInput from "../SearchInput/SearchInput";
-import {Grid} from "@mui/material";
-import {useAppSelector} from "../../hooks/useTypedSelector";
-import {ArticleModel} from "../../models/Article";
+import React, { useCallback, useState } from 'react'
+import ArticleList from '../ArticleList/ArticleList'
+import SearchInput from '../SearchInput/SearchInput'
+import { Grid } from '@mui/material'
+import { useAppSelector } from '../../hooks/useTypedSelector'
+import { ArticleModel } from '../../models/Article'
 
 const HomePage = () => {
-    const [inputValue, setInputValue] = useState('');
-    const articles = useAppSelector(state => state.articles.articles);
+  const [inputValue, setInputValue] = useState('')
+  const articles = useAppSelector((state) => state.articles.articles)
 
-    const onChangeInput = useCallback(
-        (event: React.ChangeEvent<HTMLInputElement>) => {
-            setInputValue(event.target.value.trimStart());
-        },
-        []
-    );
+  const onChangeInput = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value.trimStart())
+  }, [])
 
-    const filteredText = (text: string, values: string) => {
-        return text.toLowerCase().includes(values.toLowerCase());
-    };
+  const filteredText = (text: string, values: string) => {
+    return text.toLowerCase().includes(values.toLowerCase())
+  }
 
-    let tmpArray: string[] = [];
-    const filterUniqueValues = (item: ArticleModel) => {
-        if (tmpArray.indexOf(item.summary) === -1) {
-            tmpArray.push(item.summary);
-            return true
-        }
-        return false;
+  const tmpArray: string[] = []
+  const filterUniqueValues = (item: ArticleModel) => {
+    if (tmpArray.indexOf(item.summary) === -1) {
+      tmpArray.push(item.summary)
+      return true
     }
+    return false
+  }
 
-    const filteredArticles = (articles: ArticleModel[], value: string) => {
-        const filteredByTitle = articles.filter(({ title }) =>
-            filteredText(title, value)
-        );
+  const filteredArticles = (articles: ArticleModel[], value: string) => {
+    const filteredByTitle = articles.filter(({ title }) => filteredText(title, value))
 
-        const filteredBySummary = articles.filter(({ summary }) =>
-            filteredText(summary, value)
-        );
+    const filteredBySummary = articles.filter(({ summary }) => filteredText(summary, value))
 
-        const filteredArticles = [...filteredBySummary, ...filteredByTitle]
-        return filteredArticles.filter((item) => filterUniqueValues(item))
-    };
+    const filteredArticles = [...filteredBySummary, ...filteredByTitle]
+    return filteredArticles.filter((item) => filterUniqueValues(item))
+  }
 
-    let articlesToRender;
+  let articlesToRender
 
-    (inputValue)
-        ? (articlesToRender = filteredArticles(articles, inputValue))
-        : (articlesToRender = articles);
+  inputValue
+    ? (articlesToRender = filteredArticles(articles, inputValue))
+    : (articlesToRender = articles)
 
-    return (
-        <Grid
-            container
-            direction="column"
-            justifyContent="center"
-            alignItems="center"
-        >
-            <SearchInput
-                onChange={onChangeInput}
-                value={inputValue}
-            />
-            <ArticleList articlesToRender={articlesToRender}/>
-        </Grid>
-    );
-};
+  return (
+    <Grid container direction='column' justifyContent='center' alignItems='center'>
+      <SearchInput onChange={onChangeInput} value={inputValue} />
+      <ArticleList articlesToRender={articlesToRender} />
+    </Grid>
+  )
+}
 
-export default HomePage;
+export default HomePage
